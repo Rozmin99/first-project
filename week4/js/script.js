968de77d665a0f6fa98525674ff4265617abc198
Rozmin99 <rozmin.waghu@somaiya.edu>
2020-06-23 11:50:58 +0530

    new changes taken

---
+++ week4/js/script.js
@@ -0,0 +1,70 @@
+////var x ="Hello world!"
+//var mess ="Hi 1234568799"
+
+//var message = "in global";
+//console.log("global: message ="+ message);
+//console.log("global: message ="+ mess)
+
+//var a = function() {
+//	var message = "inside a";
+//	console.log("a: message =" + message);
+//	function b(){
+//	console.log("b : message =" + message);
+//}
+//	b();
+///
+//function b(){
+//	console.log("b : message =" + message);
+//}
+//a();**
+
+//should be undefined
+//var x;
+//x = 5;
+//console.log(x);
+//if(x == undefined){
+//	console.log("x is undefined");
+//
+//x=5;
+//if (x == undefined){
+//	console.log("x is undefined");
+//}
+//else{
+//	console.log("x has been defined");
+//}
+//string operation
+//var string = "Hello";
+//string +=" world";
+//string  += "1233";
+//string = string +"world"
+//console.log(string + "!");
+
+//math operation
+
+//console.log((5+4)/3);
+//console.log(undefined / 5);
+//function test1 (a){
+//	console.log(a / 5);
+//}
+//test1();
+
+//*equality 
+//var x=4, y=4;
+//if (x==y) {
+//	console.log("x=4 is equal to y=4");
+//}
+//if (x==y) {
+//	console.log("x='4' is equal to y=4");
+//}
+
+//**strict equality
+//if (x === y) {
+//	console.log("strict: x='4' is equal to y=4");
+//}
+///else{
+//	console.log("strict: x='4' is NOT equal to y=4");
+//}
+
+
+//if statement false
+  if (true) {}
--- week4/script.js
+++ week4/script.js
@@ -1,1 +1,34 @@
-var x ="Hello world"
+////var x ="Hello world!"
+//var mess ="Hi 1234568799"
+
+//var message = "in global";
+//console.log("global: message ="+ message);
+//console.log("global: message ="+ mess)
+
+//var a = function() {
+//	var message = "inside a";
+//	console.log("a: message =" + message);
+//	function b(){
+//	console.log("b : message =" + message);
+//}
+//	b();
+///
+//function b(){
+//	console.log("b : message =" + message);
+//}
+//a();**
+
+//should be undefined
+var x;
+console.log(x);
+//if(x == undefined){
+	//console.log("x is undefined");
+//}
+
+//x=5;
+//if (x == undefined){
+	//console.log("x is undefined");
+//}
+//else{
+	//console.log("x has been defined");
+//}
